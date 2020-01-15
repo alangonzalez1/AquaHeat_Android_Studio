09694fc129fa8dd0a5e91bec5550239c31f68774
Alan <alan.gonzalez1@pcc.edu>
2020-01-15 11:45:51 -0800

    AquaHeat

--- AquaHeat/app/src/main/java/com/example/aquaheat/MainActivity.java
+++ AquaHeat/app/src/main/java/com/example/aquaheat/MainActivity.java
@@ -6,16 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button syncTempBtn = (Button) findViewById(R.id.syncTempBtn);
-    syncTempBtn.OnClickListener(new View.OnClickListener(){
-        @Override
-                public void onClick(View view)
-    })
-
+    private Button syncTempBtn;
     private Button setButton;
+    private TextView currentTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +22,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setButton = (Button) findViewById(R.id.setButton);
+        syncTempBtn = (Button) findViewById(R.id.syncTempBtn);
+        currentTemp = (TextView) findViewById(R.id.currentTemp);
+
         setButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openSettings();
             }
         });
+
+        syncTempBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // program the button to do something here
+                int x = randGenerate(); // generate random number between 0 and 20
+                currentTemp.setText(Integer.toString(x));
+
+            }
+
+        });
     }
+
 
     public void openSettings() {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
+    }
+
+    public int randGenerate() {
+        Random rand = new Random();
+        int result;
+        result = rand.nextInt(20);
+
+        return result;
     }
 }
