260cfc2bee53c36ded5e2ba6f9042bba37c471c2
Alan <alan.gonzalez1@pcc.edu>
2020-02-03 18:38:23 -0800

    AquaHeat

--- AquaHeat/app/src/main/java/com/example/aquaheat/MainActivity.java
+++ AquaHeat/app/src/main/java/com/example/aquaheat/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,14 +15,26 @@ import android.content.Context;
 import android.app.Notification;
 import android.app.NotificationManager;
 
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button syncTempBtn;
+    private static final String TAG = "MainActivity";
+    static int lowerBoundIntF = 65, upperBoundIntF = 85; // in Fahrenheit
+    static int lowerBoundIntC, upperBoundIntC; // in Celsius
     private Button setButton;
     private TextView currentTemp;
     private TextView lowerBound, upperBound;
+    private TextView tempUnits1, tempUnits2, tempUnits3;
+    static Dweet dweet;
+    static String thingName = "AquaHeat";
+    float avrF; // the average of the Fahrenheit temperature bounds
+    float avrC; // the average of the Celsius temperature bounds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +42,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setButton = (Button) findViewById(R.id.setButton);
-        syncTempBtn = (Button) findViewById(R.id.syncTempBtn);
+        //syncTempBtn = (Button) findViewById(R.id.syncTempBtn);
         currentTemp = (TextView) findViewById(R.id.currentTemp);
         lowerBound = (TextView) findViewById(R.id.lowerBound);
         upperBound = (TextView) findViewById(R.id.upperBound);
+        tempUnits1 = (TextView) findViewById(R.id.tempUnits1);
+        tempUnits2 = (TextView) findViewById(R.id.tempUnits2);
+        tempUnits3 = (TextView) findViewById(R.id.tempUnits3);
+        avrF = average(lowerBoundIntF, upperBoundIntF);
+        avrC = average(lowerBoundIntC, upperBoundIntC);
 
+        // sync the app upon opening it - to be added later
+
+
+        // get the Celsius equivalent values of the upper and lower bounds
+        lowerBoundIntC = (int) FtoC((float)lowerBoundIntF);
+        upperBoundIntC = (int) FtoC((float)upperBoundIntF);
+
+        // convert the value if it needs to be converted
+        if(SettingsActivity2.cFlag == 1) {
+            // the Celsius flag is set
+
+            // update the main screen to reflect the Celsius upper and lower bounds
+            lowerBound.setText(Integer.toString(lowerBoundIntC));
+            upperBound.setText(Integer.toString(upperBoundIntC));
+            tempUnits1.setText("\u2103");
+            tempUnits2.setText("\u2103");
+            tempUnits3.setText("\u2103");
+
+        }
+
+        else{
+            // the Celsius flag is not set
+
+            // update the main screen to reflect the Fahrenheit upper and lower bounds
+            lowerBound.setText(Integer.toString(lowerBoundIntF));
+            upperBound.setText(Integer.toString(upperBoundIntF));
+            tempUnits1.setText("\u2109");
+            tempUnits2.setText("\u2109");
+            tempUnits3.setText("\u2109");
+        }
         // Initialize  the bounds text to whatever is the default in the settings activity
-        lowerBound.setText(Integer.toString(SettingsActivity2.lowerBoundInt));
-        upperBound.setText(Integer.toString(SettingsActivity2.upperBoundInt));
+
 
         setButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,17 +92,76 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        syncTempBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // program the sync button to do something here
-                float x = (float) randGenerate(); // generate random number between 0 and 30
-                currentTemp.setText(Float.toString(x));
-                notificationCall(x, SettingsActivity2.lowerBoundInt, SettingsActivity2.upperBoundInt);
+        thread notificationThread = new thread();
+        new Thread(notificationThread).start();
+    }
 
+    class thread extends Thread {
+        public void run() {
+            float tempF = 0;
+            float tempC = 0;
+            while (true) {
+                try {
+                    dweet = DweetIO.getLatestDweet(thingName);
+                } catch (IOException | ParseException e) {
+                    e.printStackTrace();
+                }
+                if (dweet != null) {
+                    String string = dweet.getContent().toString();
+                    String[] splitString = string.split(":");
+                    String[] splitString2 = splitString[1].split(":");
+                    String[] splitString3 = splitString2[0].split(",");
+                    String tempFstring = splitString3[0];
+                    tempF = Float.parseFloat(tempFstring);
+                    tempC = FtoC(tempF);
+
+                }
+                // format the floats to one decimal place
+                DecimalFormat decimalFormat = new DecimalFormat("#.0");
+                String outputF = decimalFormat.format(tempF);
+                String outputC = decimalFormat.format(tempC);
+                Log.d(TAG, outputF); // display the Fahrenheit value fetched
+                Log.d(TAG, outputC); // display the Celsius version of the fetched value
+                final String finalTempF = outputF;
+                final String finalTempC = outputC;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(SettingsActivity2.cFlag == 1){
+                            // Celsius flag is on
+                            // display the Celsius value instead
+                            currentTemp.setText(finalTempC);
+                        }
+                        else{
+                            // Celsius flag is not on
+                            currentTemp.setText(finalTempF);
+                        }
+                    }
+                });
+                //CHECK FOR OUT-OUT-BOUNDS
+                notificationCall(tempF, lowerBoundIntF, upperBoundIntF);
+
+                try {
+                    Thread.sleep(1000);
+                }
+                catch (InterruptedException ex) {}
+
+                //SEND UPPER AND LOWER BOUNDS
+                JsonObject json= new JsonObject();
+                json.addProperty("tempMin", lowerBoundIntF);
+                json.addProperty("tempMax", upperBoundIntF);
+                try {
+                    DweetIO.publish("AquaHeatSettings", json);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    Thread.sleep(1000);
+                }
+                catch (InterruptedException ex) {}
             }
-
-        });
+        }
     }
 
     public void notificationCall(float value, int lowerBound, int upperBound) {
@@ -86,5 +193,17 @@ public class MainActivity extends AppCompatActivity {
         result = rand.nextInt(30);
 
         return result;
+    }
+
+    public float average(int num1, int num2) {
+        return (((float) num1 + (float) num2) / 2);
+    }
+
+    public double CtoF(int num) {
+        return ((1.8 * (float) num) + 32.0);
+    }
+
+    public float FtoC(float num) {
+        return (float) ((num - 32.0) * (5.0/9.0));
     }
 }
--- AquaHeat/app/src/main/java/com/example/aquaheat/SettingsActivity2.java
+++ AquaHeat/app/src/main/java/com/example/aquaheat/SettingsActivity2.java
@@ -3,19 +3,25 @@ package com.example.aquaheat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.text.TextUtils;
+import android.widget.Switch;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class SettingsActivity2 extends AppCompatActivity {
 
-    static int lowerBoundInt = 45, upperBoundInt = 60;
-    private EditText uBndInput;
-    private EditText lBndInput;
-    private Button applyBtn;
+    static int cFlag; // celsius flag
+    private EditText uBndInput; // input for the upper bound value
+    private EditText lBndInput; // input for the lower bound value
+    private Switch Celsius; // switch to turn on the Celsius values
+    private Button applyBtn; // button to apply the changes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +32,61 @@ public class SettingsActivity2 extends AppCompatActivity {
         uBndInput = (EditText) findViewById(R.id.uBndInput);
         lBndInput = (EditText) findViewById(R.id.lBndInput);
         applyBtn = (Button) findViewById(R.id.applyBtn);
+        Celsius = (Switch) findViewById(R.id.Celsius);
+
+
+        // save switch state in shared preferences
+        SharedPreferences sharedPreferences = getSharedPreferences("save", MODE_PRIVATE);
+        Celsius.setChecked(sharedPreferences.getBoolean("value", false));
+
+        Celsius.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // what the switch will do
+                if(Celsius.isChecked())
+                {
+                    // when the switch is checked
+                    SharedPreferences.Editor editor = getSharedPreferences("save", MODE_PRIVATE).edit();
+                    editor.putBoolean("value", true);
+                    editor.apply();
+                    Celsius.setChecked(true);
+                    cFlag = 1; // toggle the celsius flag
+                }
+                else
+                {
+                    // when the switch is not checked
+                    SharedPreferences.Editor editor = getSharedPreferences("save", MODE_PRIVATE).edit();
+                    editor.putBoolean("value", false);
+                    editor.apply();
+                    Celsius.setChecked(false);
+                    cFlag = 0; // turn off the celsius flag
+                }
+            }
+        });
 
         applyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // code for what the button will do
-                lowerBoundInt = Integer.valueOf(lBndInput.getText().toString());
-                upperBoundInt = Integer.valueOf(uBndInput.getText().toString());
+                // code for what the "apply changes" button will do
 
+                // update the upper and lower bound variables if there is any user input
+                // do nothing if they are empty
+                if(TextUtils.isEmpty(uBndInput.getText().toString())) {
+                }
+                else
+                    MainActivity.upperBoundIntF = Integer.valueOf(uBndInput.getText().toString());
+
+                if(TextUtils.isEmpty(lBndInput.getText().toString())){
+                }
+                else
+                    MainActivity.lowerBoundIntF = Integer.valueOf(lBndInput.getText().toString());
+
+                // done updating, now get back to the main screen
                 openMain();
 
             }
+
+
         });
     }
 
